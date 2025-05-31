854c73019910f8f60760484b9238a8de965fb9bf
Daniel Manalu <danielmanalu2020@gmail.com>
2025-05-31 20:39:42 +0700

    Update Torus.java

--- uap/models/Torus.java
+++ uap/models/Torus.java
@@ -58,8 +58,8 @@ public class Torus extends Shape implements ThreeDimensional, PiRequired, MassCa
     @Override
     public void printInfo() {
         System.out.println("=============================================");
-        System.out.printf("Volume                : %.0f\n", getVolume());
-        System.out.printf("Luas permukaan        : %.0f\n", getSurfaceArea());
+        System.out.printf("Volume                : %.2f\n", getVolume());
+        System.out.printf("Luas permukaan        : %.2f\n", getSurfaceArea());
         System.out.printf("Massa                 : %.2f\n", getMass());
         System.out.printf("Massa dalam kg        : %.2f\n", gramToKilogram());
         System.out.printf("Biaya kirim           : Rp %.0f\n", calculateCost());
